8874336daa5beb6768561c046978d11bbb02c08d
ksaurabh7877 <k.saurabh7877@gmail.com>
2022-04-25 12:27:08 +0530

    2nd project
    2

---
+++ scripts/movies.js
@@ -0,0 +1,6 @@
+// Implement debouncing for network request
+// On clicking book now store the selected movie in localstorage as key "movie"
+// so that you can retrive it on checkout.html page
+
+      src="http://www.omdbapi.com/?apikey=[yourkey]&"
+    
